c06b83ed662e68b4768b5f24f50d8cf69ce88b02
User <User@USER-7HGTF2IC7B>
2024-05-02 15:42:16 +0400

    Добавьте файлы проекта.

---
+++ Game-Tanks-Clash/Program.cs
@@ -0,0 +1,11 @@
+﻿using System;
+namespace UlearnGame;
+public class TanksClash
+{
+    public static void Main() 
+    { 
+        var game = new TanksClash();
+        Console.WriteLine("Hello, world!");
+    }
+
+}
