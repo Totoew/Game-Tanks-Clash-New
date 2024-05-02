73dd56d03556dd7825f72d30fc787f6f2e03610e
Totoew <totoevdanil57@gmail.com>
2024-05-02 16:00:02 +0400

    ok

--- Game-Tanks-Clash/Program.cs
+++ Game-Tanks-Clash/Program.cs
@@ -4,8 +4,8 @@ public class TanksClash
 {
     public static void Main() 
     { 
-        var game = new TanksClash();
-        Console.WriteLine("Hello, world!");
+       
+       
         for (int i = 0; i < 10; i++)
         {
             Console.WriteLine(i);
